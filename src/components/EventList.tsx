936cfbd4c591a4a0df08ae678235b7bfffaa26fb
Fabricio Pavano <fpavano-eastrock@jungroup.com>
2025-01-20 13:23:04 -0300

    added basic styling + some descripiton based on event type

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,15 +1,37 @@
 import { GithubEvent } from '../types/types'
-
+import './EventList.css'
 interface EventListProps {
   events: GithubEvent[]
 }
 
 export default function EventList({ events }: EventListProps) {
+  const getEventDescription = (event: GithubEvent) => {
+    switch (event.type) {
+      case 'PushEvent':
+        return `pushed ${event.payload.commits?.length || 0} commits`
+      case 'PullRequestEvent':
+        return `${event.payload.action} a pull request`
+      case 'IssuesEvent':
+        return `${event.payload.action} an issue`
+      case 'CreateEvent':
+        return `created a ${event.payload.ref_type || 'repository'}`
+      case 'DeleteEvent':
+        return `deleted a ${event.payload.ref_type}`
+      default:
+        return `performed a ${event.type}`
+    }
+  }
+
   return (
-    <div>
+    <div className="event-list">
       {events.map((event) => (
-        <div key={event.id}>
-          <p>{event.type} by {event.actor.login} at {new Date(event.created_at).toLocaleString()}</p>
+        <div key={event.id} className="event-item">
+          <img src={event.actor.avatar_url} alt="" className="avatar" />
+          <strong>{event.actor.login + ' '} </strong>
+          {getEventDescription(event)}
+          <div className="timestamp">
+            on {event.repo.name} at {new Date(event.created_at).toLocaleString()}
+          </div>
         </div>
       ))}
     </div>
